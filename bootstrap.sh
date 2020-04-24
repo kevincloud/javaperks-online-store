a89b8924985761c5cb6ab32ea464b859d65416df
Kevin Cochran <kcochran@hashicorp.com>
2020-04-23 20:54:30 -0400

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+apachectl stop
+
 export KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export VAULT_TOKEN=$(curl --request POST \
   --data "{\"jwt\": \"$KUBE_TOKEN\", \"role\": \"cust-api\"}" \
   $VAULT_ADDR/v1/auth/kubernetes/login | jq -r .auth.client_token) && echo $VAULT_TOKEN
+
+echo "export VAULT_TOKEN=$VAULT_TOKEN" > /var/run/secrets/vault_token
+source /var/run/secrets/vault_token
 
 apachectl -D FOREGROUND
